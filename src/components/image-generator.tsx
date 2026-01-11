4d4815f461f822b553948085b93873fc5d6e8184
qwq202 <18022929563@163.com>
2026-01-11 12:46:35 +0800

    Use system fonts and tighten layout

--- src/components/image-generator.tsx
+++ src/components/image-generator.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
